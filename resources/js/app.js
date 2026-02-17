73eca96ae2fc175c683bb25f861f308b613b6ab3
William-Almeida <williamkillerca@hotmail.com>
2026-02-17 13:19:38 -0300

    Expose Sortable globally for inline Livewire component scripts

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,8 @@
+import Sortable from 'sortablejs';
+
+// Expose Sortable globally so inline Livewire component scripts can use it
+window.Sortable = Sortable;
+
 // import Alpine from 'alpinejs';
 
 // window.Alpine = Alpine;
